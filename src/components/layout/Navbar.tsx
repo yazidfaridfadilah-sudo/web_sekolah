fc8ac11e47c82d10384969bd31dfcbfe3702959e
putranfl <mputra031101@gmail.com>
2026-09-02 15:41:32 +0700

    update kapital nama sekolah

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-800 text-white py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">Smk Al-muhadjirin</h1>
+        <h1 className="text-xl font-bold">SMK AL-Muhadjirin</h1>
         <ul className="flex space-x-4">
           <li>
             <a href="#home" className="hover:text-gray-300">
