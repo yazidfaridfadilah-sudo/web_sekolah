a070edf342dd45359972a0b15f69aea9e83fd5ba
putranfl <mputra031101@gmail.com>
2026-09-03 10:39:53 +0700

    memindahkan folder images ke public

--- src/components/sections/Hero/index.tsx
+++ src/components/sections/Hero/index.tsx
@@ -1,7 +1,14 @@
 export default function Hero() {
   return (
-    <section className="min-h-screen flex items-center justify-center">
-      <h1>Hero Section - TODO</h1>
+    <section className="min-h-screen">
+      <img
+        src="fotoSekolah.jpg"
+        className="flex justify-self-end  w-100 mt-15 mr-20"
+      />
+      <div className="text-3xl mb-50 ml-20">
+        <p>SMK AL</p>
+        <p>MUHADJIRIN 1</p>
+      </div>
     </section>
   );
 }
