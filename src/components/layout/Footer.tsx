20c246899017d137909bf38e815a26ffcf5ad3db
putranfl <mputra031101@gmail.com>
2026-09-03 13:25:41 +0700

    update footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,18 +1,63 @@
+import {
+  FaInstagram,
+  FaFacebook,
+  FaYoutube,
+  FaTiktok,
+} from "react-icons/fa";
+
 export default function Footer() {
   return (
-
-    <div className="">
-      <div className=" flex bg-amber-400 text-white py-10 ">
-        <div className="ml-10">
-          <p className="text-white font-extrabold text-base">SMK1</p>
-          <div className="mt-4 text-sm text-justify font font-medium">
-            <p>Sekolah adalah mencetak penerus bangsa</p>
-            <p>yang kualitas dan berprestasi disegala bidang</p>
-            <p>yang dapat bersaing didunia internasional</p>
+    <footer className="bg-yellow-400 px-10 md:px-20 py-16">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center md:items-start gap-12">
+        <div className="text-white text-center md:text-left">
+          <h1 className="text-3xl font-bold mb-8">
+            SMK1
+          </h1>
+          <p className="text-lg font-medium leading-relaxed max-w-md">
+            Sekolah adalah tempat mencetak penerus bangsa
+            <br />
+            yang berkualitas dan berprestasi di segala bidang
+            <br />
+            yang dapat bersaing di dunia internasional
+          </p>
+        </div>
+        <div className="text-white text-center">
+          <div className="flex justify-center items-center gap-6 mb-5">
+            <a
+              href="#"
+              className="text-4xl hover:scale-110 transition-transform duration-300"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="#"
+              className="text-4xl hover:scale-110 transition-transform duration-300"
+            >
+              <FaFacebook />
+            </a>
+            <a
+              href="#"
+              className="text-4xl hover:scale-110 transition-transform duration-300"
+            >
+              <FaYoutube />
+            </a>
+            <a
+              href="#"
+              className="text-4xl hover:scale-110 transition-transform duration-300"
+            >
+              <FaTiktok />
+            </a>
+          </div>
+          <div className="text-xl md:text-2xl font-bold leading-relaxed text-justify">
+            <p>
+              Dibuat oleh Pkl_2026,
+            </p>
+            <p>
+              Menggunakan Tailwind CSS dan JS. @2026
+            </p>
           </div>
         </div>
-        <img src="ig.png" className="ml-50 w-30 h-30" />
       </div>
-    </div>
+    </footer>
   );
 }
