c930a368accf7d5e8c7ac2535c2b7344d72d6885
putranfl <mputra031101@gmail.com>
2026-09-04 15:48:28 +0700

    menambahkan icon whatssap

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,6 +3,7 @@ import {
   FaFacebook,
   FaYoutube,
   FaTiktok,
+  FaWhatsapp,
   FaChevronUp,
 } from "react-icons/fa";
 
@@ -10,11 +11,9 @@ export default function Footer() {
   return (
     <section id="footer">
       <footer className="bg-yellow-400 px-10 py-16 md:px-20">
-        <div className="mx-auto flex max-w-7xl items-start justify-between gap-10">
+        <div className="mx-auto flex max-w-7xl items-start  gap-100">
           <div className="text-left text-white">
-            <h1 className="mb-8 text-3xl font-bold">
-              SMK1
-            </h1>
+            <h1 className="mb-8 text-3xl font-bold">SMK1</h1>
 
             <p className="max-w-md text-lg font-medium leading-relaxed">
               Sekolah adalah tempat mencetak penerus bangsa
@@ -60,6 +59,14 @@ export default function Footer() {
               >
                 <FaTiktok />
               </a>
+              <a
+                href="https://wa.me/628561831850"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-4xl transition-transform duration-300 hover:scale-110"
+              >
+                <FaWhatsapp />
+              </a>
             </div>
 
             <div className="mt-8 flex items-center w-fit gap-2 text-xl font-bold border-2 px-4 py-2 border-b-gray-100">
@@ -67,18 +74,13 @@ export default function Footer() {
                 <FaChevronUp className="text-2xl" />
                 <FaChevronUp className="-mt-3 text-2xl" />
               </div>
-             
-              <a href="#hero">
-                KEMBALI KE ATAS
-              </a>
+
+              <a href="#hero">KEMBALI KE ATAS</a>
             </div>
           </div>
 
           <div className="text-left text-white">
-
-            <h2 className="mb-5 text-2xl font-bold">
-              SITE MAP
-            </h2>
+            <h2 className="mb-5 text-2xl font-bold">SITE MAP</h2>
 
             <ul className="space-y-3 text-lg">
               <li>
@@ -123,4 +125,3 @@ export default function Footer() {
     </section>
   );
 }
-
