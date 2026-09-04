1b9b6878aee6e02caadda08c70f32b8f8102ad61
unknown <anisa.chiarani119@gmail.com>
2026-09-04 15:28:27 +0700

    mengubah gambar

--- src/components/sections/Extracurricular/index.tsx
+++ src/components/sections/Extracurricular/index.tsx
@@ -2,7 +2,7 @@ export default function Extracurricular() {
   return (
     <section id="eskul" className="bg-white py-16">
 
-      
+
       <div className="text-center mb-12">
         <h1 className="text-3xl font-bold">
           Ekstrakulikuler Di{" "}
@@ -10,7 +10,8 @@ export default function Extracurricular() {
         </h1>
 
         <h2 className="text-3xl font-bold text-yellow-400">
-          Al Muhadjirin Bekasi
+          Al Muhadjirin{""}
+          <span className="text-black"> Bekasi</span>
         </h2>
 
         <p className="text-gray-500 mt-2">
@@ -18,48 +19,42 @@ export default function Extracurricular() {
         </p>
       </div>
 
-      
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-8 px-10">
 
-        {/* CARD 1 */}
-        <div className="bg-white rounded-2xl shadow-lg p-6 text-center hover:scale-105 transition">
-          <div className="w-16 h-16 mx-auto mb-4 rounded-full bg-purple-100 flex items-center justify-center text-2xl">
-            ⚜️
-          </div>
-          <h3 className="font-semibold text-lg">praja muda karana</h3>
+      <div className="grid grid-cols-1 md:grid-cols-4 px-60">
+
+
+        <div className="bg-white rounded-2xl shadow-lg p-6 text-left w-60 hover:scale-105 transition">
+          <img src="src/components/sections/Extracurricular/Rectangle 70.jpg" alt="Praja Muda Karana" className="ml-1 w-16 h-16 mx-auto mb-4 rounded-full" />
+          <h3 className="font-semibold  text-xl">praja muda</h3>
+            <h3 className="font-semibold  text-xl">karana</h3>
           <p className="text-gray-400 text-sm mt-2">
             Pertama kali di bentuk team tahun 2020
           </p>
         </div>
 
-        
-        <div className="bg-white rounded-2xl shadow-lg p-6 text-center hover:scale-105 transition">
-          <div className="w-16 h-16 mx-auto mb-4 rounded-full bg-red-100 flex items-center justify-center text-2xl">
-            ➕
-          </div>
-          <h3 className="font-semibold text-lg">Palang merah remaja</h3>
+
+        <div className="bg-white rounded-2xl shadow-lg p-6 text-left w-60 hover:scale-105 transition">
+          <img src="src/components/sections/Extracurricular/Rectangle 80.jpg" alt="pmr" className="ml-1 w-16 h-16 mx-auto mb-4 rounded-2xl" />
+          <h3 className="font-semibold text-xl">Palang merah</h3>
+          <h3 className="font-semibold text-xl">remaja</h3>
           <p className="text-gray-400 text-sm mt-2">
             Pertama kali di bentuk team tahun 2021
           </p>
         </div>
 
-        
-        <div className="bg-white rounded-2xl shadow-lg p-6 text-center hover:scale-105 transition">
-          <div className="w-16 h-16 mx-auto mb-4 rounded-full bg-gray-200 flex items-center justify-center text-2xl">
-            🏔️
-          </div>
-          <h3 className="font-semibold text-lg">Pecinta alam</h3>
+
+        <div className="bg-white rounded-2xl shadow-lg p-6 text-left w-60 hover:scale-105 transition">
+          <img src="src/components/sections/Extracurricular/Rectangle 82.jpg" alt="gunung" className="ml-1 w-16 h-16 mx-auto mb-4 rounded-full" />
+          <h3 className="font-semibold text-xl">Pecinta alam</h3>
           <p className="text-gray-400 text-sm mt-2">
             Pertama kali di bentuk team tahun 2022
           </p>
         </div>
 
-        
-        <div className="bg-white rounded-2xl shadow-lg p-6 text-center hover:scale-105 transition">
-          <div className="w-16 h-16 mx-auto mb-4 rounded-full bg-blue-100 flex items-center justify-center text-2xl">
-            🏐
-          </div>
-          <h3 className="font-semibold text-lg">Volleyball</h3>
+
+        <div className="bg-white rounded-2xl shadow-lg p-6 text-left w-60 hover:scale-105 transition">
+         <img src="src/components/sections/Extracurricular/Rectangle 83.jpg" alt="vollyball" className="ml-1 w-16 h-16 mx-auto mb-4 rounded-full" />
+          <h3 className="font-semibold text-xl">Volleyball</h3>
           <p className="text-gray-400 text-sm mt-2">
             Pertama kali di bentuk team tahun 2023
           </p>
